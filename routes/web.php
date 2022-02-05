234d4a64eb12c854ba929848a0038f19bc2ce363
ArKlei <arunas.kleinas@gmail.com>
2022-02-05 13:28:13 +0200

    Routes ready

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,37 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::prefix('products')->group(function() {
+    //Index
+    Route::get('', 'App\Http\Controllers\ProductController@index')->name('product.index');
+    //Create
+    Route::get('create', 'App\Http\Controllers\ProductController@create')->name('product.create');
+    Route::post('store', 'App\Http\Controllers\ProductController@store' )->name('product.store');
+    // Edit form, id
+    Route::get('edit/{product}', 'App\Http\Controllers\ProductController@edit')->name('product.edit');
+    Route::post('update/{product}', 'App\Http\Controllers\ProductController@update')->name('product.update');
+    //Delete
+    Route::post('destroy/{product}', 'App\Http\Controllers\ProductController@destroy' )->name('product.destroy');
+    //Show
+    Route::get('show/{product}', 'App\Http\Controllers\ProductController@show')->name('product.show');
+
+  });
+
+  Route::prefix('product_categories')->group(function() {
+
+        //Index
+        Route::get('', 'App\Http\Controllers\ProductCategoryController@index')->name('product_category.index');
+        //Create
+        Route::get('create', 'App\Http\Controllers\ProductCategoryController@create')->name('product_category.create');
+        Route::post('store', 'App\Http\Controllers\ProductCategoryController@store' )->name('product_category.store');
+        //Edit form, id
+        Route::get('edit/{product_category}', 'App\Http\Controllers\ProductCategoryController@edit')->name('product_category.edit');
+        Route::post('update/{product_category}', 'App\Http\Controllers\ProductCategoryController@update')->name('product_category.update');
+        //Delete
+        Route::post('destroy/{product_category}', 'App\Http\Controllers\ProductCategoryController@destroy' )->name('product_category.destroy');
+        //Show
+        Route::get('show/{product_category}', 'App\Http\Controllers\ProductCategoryController@show')->name('product_category.show');
+
+  });
+
